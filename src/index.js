4bf2236604bdc4c16b0a8f494fbc2e2d58ee291e
Vitor <vitorlgomes@hotmail.com>
2020-04-23 20:45:06 -0300

    array handling

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 import React from 'react';
 
-const addChildrens = (flattedChildren, node, nodeChildren) => {
-  if (nodeChildren) {
-    return flattedChildren.push(...flattenChildren(nodeChildren));
-  } else {
-    return flattedChildren.push(node);
-  }
-};
-
 const flattenChildren = children => {
   let flattedChildren = [];
   const isArray = children && Array.isArray(children);
@@ -16,12 +8,20 @@ const flattenChildren = children => {
     children.map(node => {
       const { props: { children: nodeChildren } = {} } = node;
 
-      addChildrens(flattedChildren, node, nodeChildren);
+      if (nodeChildren) {
+        return flattedChildren.push(...flattenChildren(nodeChildren));
+      } else {
+        return flattedChildren.push(node);
+      }
     });
   } else {
     const { props: { children: nodeChildren } = {} } = children;
 
-    addChildrens(flattedChildren, children, nodeChildren);
+    if (nodeChildren) {
+      return flattedChildren.push(...flattenChildren(nodeChildren));
+    } else {
+      return flattedChildren.push(node);
+    }
   }
 
   return flattedChildren;
